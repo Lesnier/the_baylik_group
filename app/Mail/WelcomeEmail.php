8805d3d6bb6785f28a209021c3174cbf83f7cbbf
lesinnov <lesinnov@gmail.com>
2022-08-12 10:54:47 -0500

    V1.0

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class WelcomeEmail extends Mailable
+class WelcomeEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
